93ee0fca260267afb294eb26460598915f7ca34a
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-09-27 11:26:34 -0400

    The very basics laid out

--- fancy-blog-frontend/src/App.js
+++ fancy-blog-frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Switch } from 'react-router-dom'
+
+// Components
+import Title from './components/Title';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Title />
       </header>
+      <nav></nav>
+      <Switch>
+        <Route exact path='/' component={null} />  
+      </Switch>
     </div>
   );
 }
---
+++ fancy-blog-frontend/src/components/Title.js
@@ -0,0 +1,7 @@
+const Title = () => {
+  return (
+    <h1>Fancy Blog</h1>
+  )
+}
+
+export default Title
