1636c63048735aacedba8f43eadcbea99ada132d
Parker Byers <parker.n.byers@gmail.com>
2021-09-28 08:41:07 -0500

    c/p like functionality for dislike functionality.

--- fancy-blog-frontend/src/components/Comments.js
+++ fancy-blog-frontend/src/components/Comments.js
@@ -30,6 +30,27 @@ const Comments = ({match}) => {
       })
       .catch((error) => {console.error('There was an issue updating the post: ', error)})
   }
+  function dislikePost () {
+    let updatedPost = post
+    updatedPost.dislikes = updatedPost.dislikes + 1
+    fetch(`${APIURL}/${post._id}`, {
+      method: 'PUT', // put updates all keys in object
+    mode: 'cors', // cors
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+      },
+    redirect: 'follow', 
+    referrerPolicy: 'no-referrer', 
+    body: JSON.stringify(updatedPost)
+    })
+      .then(response => response.json())
+      .then(responseJSON => {
+        setPost(responseJSON)
+        // console.log('Post successfully updated: ',responseJSON)  //log it cus why not
+      })
+      .catch((error) => {console.error('There was an issue updating the post: ', error)})
+  }
 
   useEffect(() => {
     const url = `${APIURL}/${match.params.id}`
@@ -53,6 +74,7 @@ const Comments = ({match}) => {
     <div>
       <p>{post.text}</p>
       <p>Likes: {post.likes}</p>
+      <p>Dislikes: {post.dislikes}</p>
       <ul>
           {post.comments.map((comment, idx) => (
             <li key={idx}>{comment}</li>
@@ -60,7 +82,7 @@ const Comments = ({match}) => {
       </ul>
       <div>
         <button onClick={likePost} >like</button>
-        <button>dislike</button>
+        <button onClick={dislikePost} >dislike</button>
         <button>New Comment</button>
       </div>
     </div>
