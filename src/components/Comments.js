75d98f7cefe9bdf22b15a084b004b59b1acb7516
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2021-09-29 15:13:35 -0400

    add show page styling

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react';
-import { APIURL } from '../config.js';
+import { useState, useEffect } from "react";
+import { APIURL } from "../config.js";
 
 // Components
-import NewComment from './NewComment.js';
-import Buttons from './Buttons.js';
+import NewComment from "./NewComment.js";
+import Buttons from "./Buttons.js";
+
+import { Button, Card, Container } from "react-bootstrap";
 
 const Comments = ({ match, posts, setPosts }) => {
-
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
   const [newComment, setNewComment] = useState(false);
@@ -17,37 +18,48 @@ const Comments = ({ match, posts, setPosts }) => {
       .then((response) => response.json())
       .then((data) => setPost(data))
       .catch(() => {
-        setError(true)
-      })
+        setError(true);
+      });
   }, [match.params.id]);
 
   const handleComment = () => {
-    let state = newComment
+    let state = newComment;
     setNewComment(!state);
   };
 
   if (error) {
     return <div>There was a problem getting the data.</div>;
-  };
+  }
 
   if (!post) {
     return <div>Loading...</div>;
-  };
+  }
 
   return (
-    <div>
-      <p>{post.text}</p>
-      <ul>
-        {post.comments.map((comment, idx) => (
-          <li key={idx}>{comment}</li>
-        ))}
-      </ul>
-      <div>
-        <Buttons post={post} posts={posts} setPosts={setPosts} />
-        <button onClick={handleComment}>New Comment</button>
-        {!newComment ? null : <NewComment post={post} setPost={setPost} setNewComment={setNewComment} />}
-      </div>
-    </div>
+    <Container fluid>
+      <Card className="mt-3 mb-3">
+        <Card.Body>
+          <p style={{ fontSize: "19px" }}>{post.text}</p>
+
+          <div>
+            <Buttons post={post} posts={posts} setPosts={setPosts} />
+            <Button variant="outline-info" className="mb-3" onClick={handleComment}>
+              New Comment
+            </Button>
+            {!newComment ? null : <NewComment post={post} setPost={setPost} setNewComment={setNewComment} />}
+          </div>
+          <hr />
+
+          <p style={{ fontSize: "19px" }}>Comments: </p>
+          <ol>
+            {post.comments.map((comment, idx) => (
+              <li key={idx}>{comment}</li>
+            ))}
+          </ol>
+        </Card.Body>
+        <Card.Footer>{post.createdAt}</Card.Footer>
+      </Card>
+    </Container>
   );
 };
 
