087f8417fd4b70f4a58ef4518d6bb3a52e7d48bb
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-09-29 16:58:05 -0400

    Updated and styled footer

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,18 +1,16 @@
 import { Redirect } from "react-router-dom";
 
-
-
 const Random = ({ posts }) => {
 
   const getRandomPostId = () => {
-    let randomIdx = Math.floor(Math.random() * posts.length)
-    let randomPost = posts[randomIdx]
-    return randomPost._id
-  }
+    let randomIdx = Math.floor(Math.random() * posts.length);
+    let randomPost = posts[randomIdx];
+    return randomPost._id;
+  };
 
   return (
     <Redirect to={`/${getRandomPostId()}`} />
-  )
+  );
 };
 
 export default Random;
